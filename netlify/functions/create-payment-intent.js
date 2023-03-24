733b31c8433eeca45ea0ced38d28a4214c56bb71
Nick <kammer@gmail.com>
2023-03-24 12:46:37 -0400

    update

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,9 +1,4 @@
-//Reminder: To use netlify serverless functions, netlify looks for a folder "netlify" in root folder, 
-//and within "netlify" folder, it looks for "functions" folder to use during build step
-
-//We're working on the backend serverlass functions here
-
-require("dotenv").config(); //attached .env variables onto process environment
+require("dotenv").config(); 
 const stripe = require("stripe")(process.env.STRIPE_UNPUBLISHED_KEY);
 
 
@@ -15,9 +10,9 @@ exports.handler = async (event) => {
         const paymentIntent = await stripe.paymentIntents.create({
             amount,
             currency: "usd",
-            // payment_method_types: ['card']     /*Only used for CardElement, not PaymentElement*/
-            automatic_payment_methods: {  //
-                enabled: true,            //
+            
+            automatic_payment_methods: {  
+                enabled: true,            
               },
         });
         
@@ -25,7 +20,7 @@ exports.handler = async (event) => {
             statusCode: 200,
             body: JSON.stringify({paymentIntent})
             
-            //clientSecret: paymentIntent.client_secret //
+            
         }
         
     }

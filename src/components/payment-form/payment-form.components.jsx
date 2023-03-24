f614db2659c4c12e189259032dadb77ab3536d59
Nick <kammer@gmail.com>
2023-03-24 16:28:22 -0400

    update

--- src/components/payment-form/payment-form.components.jsx
+++ src/components/payment-form/payment-form.components.jsx
@@ -1,26 +1,23 @@
-import { useState, Fragment } from 'react';
+import { useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {CardElement, PaymentElement, useStripe, useElements, Elements, AddressElement} from '@stripe/react-stripe-js';
+import { PaymentElement, useStripe, useElements, AddressElement} from '@stripe/react-stripe-js';
 import { useNavigate } from 'react-router-dom';
 import { createOrderDocument } from '../../utils/firebase/firebase.utils.js';
 import { selectCartTotal, selectCartItems } from '../../store/cart/cart.selector.js';
-import { selectCurrentUser } from '../../store/user/user.selector.js';
 import {OUR_BUTTON_TYPE_CLASSES} from '../button/button.components.jsx';
 import { emptyItemsFromCart } from '../../store/cart/cart.action.js';
 import {PaymentFormContainer, FormContainer, PaymentButton} from './payment-form.styles.jsx';
 import {setOrderId} from '../../store/orders/orders.action.js';
-import { stripePromise, callResponse, callClientSecret } from '../../utils/stripe/stripe.utils.js';
+
 
 
 const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
     const amountPaid = useSelector(selectCartTotal);
-    const currentUser = useSelector(selectCurrentUser);
-    
+        
     const [isProcessingPayment, setIsProcessingPayment] = useState(false);
-    const [stripeOptions, setStripeOptions] = useState("");
-    const [emailAddress, setEmailAddress] = useState(null);
+
     const cartItems = useSelector(selectCartItems);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -28,42 +25,20 @@ const PaymentForm = () => {
 
 
     const paymentHandler = async (e) => {
-        e.preventDefault(); //prevents normal default actions of form submission
+        e.preventDefault(); 
         
         
         if (!stripe || !elements) {
             return;
         }
         setIsProcessingPayment(true);
-    //
+    
         const addressElement = elements.getElement('address');
 
         const {complete, value} = await addressElement.getValue();
         const shippingAddress = value;
         if (complete) {
-            // Allow user to proceed to the next step
-            // Optionally, use value to store the address details
-        
-    //
-            // const response = await fetch('/.netlify/functions/create-payment-intent', {
-            //     method: 'post',
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     },
-            //     body: JSON.stringify({amount: amountPaid*100})  /* total amount paid is in cents */
-            // }).then(res => {
-            //     return res.json();
-            // });
             
-            // const clientSecret = await response.paymentIntent.client_secret;
-            
-            // let response = callResponse(amountPaid)
-            // response.then((res)=>setStripeOptions(res))
-            
-            // setStripeOptions(clientSecret);
-
-            // console.log("secret",typeof clientSecret);
-            // console.log("options",stripeOptions);
 
             const paymentResult = await stripe.confirmPayment({
                 elements,
@@ -74,14 +49,7 @@ const PaymentForm = () => {
                 redirect: 'if_required',
             });
             
-            // const paymentResult = await stripe.confirmCardPayment(clientSecret, {
-            //     payment_method: {
-            //         card: elements.getElement(CardElement),
-            //         billing_details: {
-            //             name: currentUser ? currentUser.displayName : 'Guest'
-            //         }
-            //     }
-            // });
+            
             
             
             setIsProcessingPayment(false);
@@ -116,7 +84,7 @@ const PaymentForm = () => {
         <PaymentFormContainer>
             <h2>Shipping Address:</h2>
                     
-                <FormContainer onSubmit={paymentHandler}> {/* This FormContainer makes the Button in it a submission button automatically */}
+                <FormContainer onSubmit={paymentHandler}> 
                 <h3>Shipping</h3>
                 <AddressElement options={{mode: 'shipping'}} />
                 {/* <EmailInput  type="email" name='reply_to' placeholder="Your Email Address" value={toSend.reply_to} onChange={handleChange} /> */}
